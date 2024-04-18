e33c6d5716ba4cc6b42029d429b96ca0e300afb1
Edson <ewiyanto@student.ciputra.ac.id>
2024-04-18 12:40:56 +0700

    Initial Commit

---
+++ 0706012210054-Edson-AFL3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  0706012210054-Edson-AFL3
+//
+//  Created by student on 18/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 0706012210054-Edson-AFL3/_706012210054_Edson_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012210054_Edson_AFL3App.swift
+//  0706012210054-Edson-AFL3
+//
+//  Created by student on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct _706012210054_Edson_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
